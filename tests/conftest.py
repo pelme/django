a3bf61d3edc1e14dc1868df49293342ebc4e3b34
Andreas Pelme <andreas@pelme.se>
2015-02-08 16:53:31 +0100

    pytest: Add the tests directory to sys.path

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,13 @@
 import os
+import sys
 import tempfile
+
+from runtests import RUNTESTS_DIR
+
+
+def pytest_configure():
+    sys.path.insert(0, RUNTESTS_DIR)
+
 
 def pytest_sessionstart():
     TEMP_DIR = tempfile.mkdtemp(prefix='django_')
