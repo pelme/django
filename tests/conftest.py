896f2b86ebc92c965ed8c67e52126141cf7c8214
Andreas Pelme <andreas@pelme.se>
2015-02-08 16:54:09 +0100

    pytest: Ignore postgres tests for non postgres settings

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,8 +5,15 @@ import tempfile
 from runtests import RUNTESTS_DIR
 
 
+collect_ignore = []
+
 def pytest_configure():
     sys.path.insert(0, RUNTESTS_DIR)
+
+    from django.db import connection
+
+    if connection.vendor != 'postgresql':
+        collect_ignore.extend(['postgres_tests', 'postgres'])
 
 
 def pytest_sessionstart():
